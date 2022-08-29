8350200420cbd11610d9db3b0ac32a70ea7f0ed8
Guillaume Chaumont <112415471+guillomn-git@users.noreply.github.com>
2022-08-29 19:48:19 +0300

    Add files via upload

---
+++ Excercise 1/src/main/java/com/example/demo/ContactController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+
+public class ContactController {
+	@RequestMapping("/contact")
+
+	public String index() {
+
+		return "This is the contact page";
+	}
+}
---
+++ Excercise 1/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+
+public class HelloController {
+	@RequestMapping("/hello")
+
+	public String hello(@RequestParam(defaultValue = "website", name = "location", required = false) String location,
+			@RequestParam(defaultValue = "guest", name = "name", required = false) String name) {
+
+		return "Welcome to the " + location + " " + name + "!";
+	}
+}
